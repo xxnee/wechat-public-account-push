0c96c796d78b78c9e98fc9940ed9d3023c9c95f7
xxnee <114174268+xxnee@users.noreply.github.com>
2022-09-23 00:37:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -66,9 +66,9 @@ export const USER_CONFIG = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "0CuPzi8HqxtEQKqYOxctqo__Qlue0cZtRfCp22pKQRA",
       // 所在省份
-      province: "福建",
+      province: "广东",
       // 所在城市
-      city: "福州",
+      city: "肇庆",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '01-30',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
