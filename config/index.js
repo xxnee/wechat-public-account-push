4476803be327f4bccd933553310482d5ae4cb919
xxnee <114174268+xxnee@users.noreply.github.com>
2022-09-23 00:30:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,15 +62,15 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oAI0X6g2k3I_hc1hqnrFmNq-hWZo",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "0CuPzi8HqxtEQKqYOxctqo__Qlue0cZtRfCp22pKQRA",
       // 所在省份
-      province: "广东",
+      province: "福建",
       // 所在城市
-      city: "肇庆",
+      city: "福州",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-30',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
