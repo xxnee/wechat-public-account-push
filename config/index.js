ac7bf3e4d980d36f77f10dc17b97c28f3d3ad321
xxnee <114174268+xxnee@users.noreply.github.com>
2022-09-25 16:32:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -104,6 +104,10 @@ export const USER_CONFIG = {
       horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+//         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "你", "year": "2002", "date": "11-24"},
         {"type": "生日", "name": "宝贝星诺", "year": "2001", "date": "01-30"},
       ],
